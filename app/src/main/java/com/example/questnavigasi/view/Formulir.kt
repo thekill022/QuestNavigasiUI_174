97559cfcb0c22181017f5ae0b636c41f2f534d19
thekill022 <gibfathoni@gmail.com>
2025-11-07 09:44:59 +0700

    membuat composable function untuk Formulir.kt dengan konstanta jenisK dan function OnSubmitOnClick

--- app/src/main/java/com/example/questnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasi/view/Formulir.kt
@@ -1,2 +1,10 @@
 package com.example.questnavigasi.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK : List<String> = listOf("Laki-Laki", "Perempuan"),
+    OnSubmitOnClick : () -> Unit
+) {
+}
