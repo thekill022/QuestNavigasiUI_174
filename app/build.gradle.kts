118adf9f44845b251bbc08eac1ed75aeee9ff1f1
thekill022 <gibfathoni@gmail.com>
2025-11-07 14:16:35 +0700

    menyesuaikan androidx.compose:compose-bom dengan versi yang lebih stablle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,10 +51,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.activity:activity-compose:1.11.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
