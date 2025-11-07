3d52f4338104fbef17ada0deef18749475ce1a0b
thekill022 <gibfathoni@gmail.com>
2025-11-07 14:06:14 +0700

    membuat state management untuk aplikasi

--- app/src/main/java/com/example/questnavigasi/view/navigasi.kt
+++ app/src/main/java/com/example/questnavigasi/view/navigasi.kt
@@ -1,8 +1,49 @@
 package com.example.questnavigasi.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
     Formulirku,
     Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { isiRuang->
+            NavHost(
+                navController = navController,
+                startDestination = Navigasi.Formulirku.name,
+                modifier = Modifier.padding(paddingValues = isiRuang),
+            ) {
+                composable(
+                    route = Navigasi.Formulirku.name
+                ) {
+                    FormIsian(
+//                        pilihanJK = jenisK.map{id -> konteks.resouces.getString(id)}
+                        OnSubmitOnClick = {
+                            navController.navigate(route = Navigasi.Detail.name)
+                        }
+                    )
+                }
+                composable(route = Navigasi.Detail.name) {
+                    TampilData(
+                        onBackBtnClick = {
+//                            nanti akan kita isikan function
+                        }
+                    )
+                }
+            }
+    }
 }
