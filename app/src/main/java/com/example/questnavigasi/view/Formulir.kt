54d80bbfe0d79d6358a0415196cf8b8caa919374
thekill022 <gibfathoni@gmail.com>
2025-11-07 10:21:13 +0700

    menambahkan textfiled dan button submit

--- app/src/main/java/com/example/questnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasi/view/Formulir.kt
@@ -2,12 +2,18 @@ package com.example.questnavigasi.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -53,9 +59,43 @@ fun FormIsian(
                     onValueChange = {}
                 )
                 Divider(
-                    modifier = Modifier.padding(20.dp).width(250.dp),
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .width(250.dp),
                     thickness = DividerDefaults.Thickness, color = Color.Red
                 )
+                Row {
+                    jenisK.forEach { item ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(selected = false, onClick = { item })
+                            Text(text = item)
+                        }
+                    }
+                }
+                Divider(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .width(250.dp), 
+                    thickness = 1.dp, 
+                    color = Color.Red,
+                )
+                OutlinedTextField(
+                    value = "",
+                    singleLine = true,
+                    modifier = Modifier.width(250.dp),
+                    label = { Text(text = "Alamat")},
+                    onValueChange = {}
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(1f)
+                        .padding(25.dp),
+                    onClick = OnSubmitOnClick) {
+                    Text(text = stringResource(id = R.string.submit))
+                }
             }
     }
     
