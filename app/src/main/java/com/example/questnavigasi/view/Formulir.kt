8997786efecfc38f03706d2a299a8be7a86f5380
thekill022 <gibfathoni@gmail.com>
2025-11-07 09:59:18 +0700

    menambahkan outlinedtextfield dan divider

--- app/src/main/java/com/example/questnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasi/view/Formulir.kt
@@ -3,7 +3,11 @@ package com.example.questnavigasi.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasi.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,6 +43,19 @@ fun FormIsian(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                OutlinedTextField(
+                    value = "", 
+                    singleLine = true,
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .width(250.dp),
+                    label = { Text(text = "Nama Lengkap")},
+                    onValueChange = {}
+                )
+                Divider(
+                    modifier = Modifier.padding(20.dp).width(250.dp),
+                    thickness = DividerDefaults.Thickness, color = Color.Red
+                )
             }
     }
     
