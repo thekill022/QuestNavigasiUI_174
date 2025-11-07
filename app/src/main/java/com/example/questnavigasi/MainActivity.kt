ea64149846653702897aae6fb60576c56bf716df
thekill022 <gibfathoni@gmail.com>
2025-11-07 14:10:15 +0700

    memanggil DataApp di main untuk dijalankan

--- app/src/main/java/com/example/questnavigasi/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasi/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questnavigasi.ui.theme.QuestNavigasiTheme
+import com.example.questnavigasi.view.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DataApp()
                 }
             }
         }
