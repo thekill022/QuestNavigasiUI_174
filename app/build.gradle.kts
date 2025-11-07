f9426a9ff6bb776c3dd872a79e30840f19a4e55f
thekill022 <gibfathoni@gmail.com>
2025-11-07 14:17:52 +0700

    menyesuaikan androidx.naviagation dengan versi yang lebih stablle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
