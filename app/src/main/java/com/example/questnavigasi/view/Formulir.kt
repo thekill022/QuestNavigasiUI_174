fcf6a5b2bc3d1e1044ead9e410bbbd3d4accedd0
thekill022 <gibfathoni@gmail.com>
2025-11-07 09:51:44 +0700

    menambahkan topbar

--- app/src/main/java/com/example/questnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasi/view/Formulir.kt
@@ -1,10 +1,34 @@
 package com.example.questnavigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasi.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
     jenisK : List<String> = listOf("Laki-Laki", "Perempuan"),
     OnSubmitOnClick : () -> Unit
 ) {
+    
+    Scaffold(
+        modifier = Modifier,
+        topBar = {TopAppBar(
+            title = { Text(stringResource(id = R.string.home), color = Color.White)},
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                colorResource(id = R.color.teal_200)
+            )
+        )}
+    ) {
+        
+    }
+    
 }
