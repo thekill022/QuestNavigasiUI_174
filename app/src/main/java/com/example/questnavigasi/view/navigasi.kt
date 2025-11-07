57f788e3620acd6181579ec0e85b73ef315144ae
thekill022 <gibfathoni@gmail.com>
2025-11-07 14:09:43 +0700

    menghapus package yang tidak perlu

--- app/src/main/java/com/example/questnavigasi/view/navigasi.kt
+++ app/src/main/java/com/example/questnavigasi/view/navigasi.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
